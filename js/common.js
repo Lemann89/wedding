53623553e63795152653d0aeb32cce9271ca356d
Vladislav <59619468+Lemann89@users.noreply.github.com>
2020-04-22 22:45:15 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -3,6 +3,21 @@ const burger = document.querySelector('.nav__burger');
 
 burger.addEventListener('click', () => {
 	nav.classList.toggle('nav__active');
-	burger.classList.toggle('nav__burger--active');
+	burger.classList.toggle('nav__burger-active');
 	document.body.classList.toggle('lock');
 });
+
+console.log(document.body.clientWidth);
+
+
+if(document.body.clientWidth <= 768){
+	$(document).ready(function(){
+		$('.services__cards').slick({
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			variableWidth:true,
+			dots: true,
+			arrows: false
+		});
+	});
+}
